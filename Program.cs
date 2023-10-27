3396d2ae369a3412a543fde9fe0cf998450765e7
danye16 <danyel.parracarrillo@gmail.com>
2023-10-27 07:47:06 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenUnidad2_JDPC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}AUb4
